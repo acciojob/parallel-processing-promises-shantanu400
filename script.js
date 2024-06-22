f227a821838da1d34563be95c10caa57b86ba846
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-22 18:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.onclick=function(){
+	const promiseall=images.map((img)=>{
+		return new Promise((res,rej)=>{
+		setTimeout((res,rej)=>{
+			res(images.url);
+			rej(`Failed to load image's URL: ${image.url}`)
+		})
+	})
+	})
+		
+}
+
+Promise.all((promiseall)=>{
+	output.innerHTML=``
+	promiseall.forEach((img)=>{
+		output.innetHTML+=`
+		<img src="img.url">
+		`
+	})
+}).catch(error)=>{
+	console.error(error);
+};
+
